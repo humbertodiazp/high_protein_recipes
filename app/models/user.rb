f84886ba2acaebb3da43dadbfc0cfd4da6a47726
Humberto <hdkaratechop@gmail.com>
2023-07-08 16:02:36 -0700

    Validate unique username/email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:email, :username]
+          :recoverable, :rememberable, :validatable, :authentication_keys => [:email, :username]
+
+  validates_uniqueness_of :username, :email 
+  
 end
