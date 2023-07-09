4507293c9d75d8a9bacb5308a38154fb1bddb7c4
Humberto <hdkaratechop@gmail.com>
2023-07-08 22:36:18 -0700

    Add new parameters to allow Google sign in

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_003005) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_004341) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
