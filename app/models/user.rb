4c99d103eb109ddb31244506e807345ecbb4ed66
Humberto <hdkaratechop@gmail.com>
2023-07-10 20:07:17 -0700

    Set up omniauth for google api login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email 
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email #assuming the user model has an email
+      user.password = Devise.friendly_token[0,20] #random password for the user
+      user.full_name = auth.info.name #assuming the user model has a name
+      user.avatar_url = auth.info.image #assuming the user model has an image
+      #if you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
 end
