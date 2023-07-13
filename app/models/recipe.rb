16ec52d367f0be0f1f30663890701900232427da
Humberto <hdkaratechop@gmail.com>
2023-07-12 22:53:59 -0700

    Add validations/ relationship to ingredients model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    has_many :ingredients
+    accepts nested atrributes_for :ingredients, 
+    reject_if: :all_blank, allow_destroy: true
+    belongs_to :user
+    validates :title, :description presence :true
+    validates :title, length: { minimum: 5 }
+    validates :description, length: { minimum: 10 }
 end
