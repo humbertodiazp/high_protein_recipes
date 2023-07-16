0b427a787239ade1c7bba0dac2de62bbc192e8c5
Humberto <hdkaratechop@gmail.com>
2023-07-16 15:58:11 -0700

    Clean up controller and add setup recipe method

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,16 @@
 class IngredientsController < ApplicationController
-    def destroy
-        @project = Project.new(tasks: [Task.new])
+    before_action :setup_recipe 
+
+    def new
     end
+  
+    def destroy
+    end
+
+    private
+  
+    def setup_recipe
+      @recipe = Recipe.new(ingredients: [Ingredient.new])
+    end
+  end
 end
