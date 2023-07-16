aaad0bfaef22e8c748d8c0100919603e686b18b1
Humberto <hdkaratechop@gmail.com>
2023-07-15 21:48:09 -0700

    Add ingredients to new recipe action

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
     has_many :ingredients
-    accepts nested atrributes_for :ingredients, 
+    accepts_nested_attributes_for :ingredients,
     reject_if: :all_blank, allow_destroy: true
     belongs_to :user
-    validates :title, :description presence :true
+    validates :title, :instructions, presence: true
     validates :title, length: { minimum: 5 }
-    validates :description, length: { minimum: 10 }
+    validates :instructions, length: { minimum: 10 }
 end
