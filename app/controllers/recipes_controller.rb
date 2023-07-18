a191c11f025dce66d7afa27cffef467fa6d1bdb3
Humberto <hdkaratechop@gmail.com>
2023-07-17 22:20:21 -0700

    Add user to create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,49 +1,69 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
 
   def index
     @recipes = Recipe.all
-    respond_with(@recipes)
   end
 
   def show
-    respond_with(@recipe)
+
   end
 
   def new
-    @recipe = Recipe.new( ingredients: [Ingredient.new])
-    respond_with(@recipe)
+    @recipe = Recipe.new(ingredients: [Ingredient.new])
   end
 
   def edit
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.save
-    respond_with(@recipe)
+    @recipe = current_user.recipes.new(recipe_params)
+  
+      if @recipe.save
+        redirect_to @recipe, notice: "Recipe was successfully created."
+      else
+        @recipe = current_user.recipes.new(recipe_params)
+        render 'index', status: :unprocessable_entity
+      end
   end
+
+
+
+  
 
   def update
     @recipe.update(recipe_params)
-    respond_with(@recipe)
+  end
+
+  def update
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @recipe.destroy
-    respond_with(@recipe)
+
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
-    def set_recipe
+  def set_recipe
       @recipe = Recipe.find(params[:id])
-    end
+  end
 
-    def recipe_params
-      params.require(:recipe).
-      permit(:name, :instructions, ingredients_attributes:
-        [:id, :description, :_destroy])
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :instructions, ingredients_attributes:[:id, :description, :_destroy])
+  end
 end
+
