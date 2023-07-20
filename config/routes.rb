7dbddc0166bd45fd02fd8aa871a95d8fee14eb0f
Humberto <hdkaratechop@gmail.com>
2023-07-19 21:22:48 -0700

    Add create ingredient route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root 'recipes#home'
-  
-  resources :recipes 
-  
-  
+
   resources :ingredients, only: [], param: :index do
     member do
       delete '(id)' => 'ingredients#destroy', as: ""
+      post '/' => 'ingredients#create'
     end
   end
+  
+  
+  resources :recipes 
+  resources :users, only: %i[index]
 
   devise_for :users,
               controllers: {
@@ -16,8 +18,10 @@ Rails.application.routes.draw do
                 sessions: 'users/sessions',
                 omniauth_callbacks: 'users/omniauth_callbacks'
                 }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
