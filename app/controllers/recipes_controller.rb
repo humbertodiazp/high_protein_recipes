6ae4ae0099232271e0053b75368cccccddcce6c2
Humberto <hdkaratechop@gmail.com>
2023-07-24 02:24:01 -0700

    Generate recipe and ingredients model/controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,48 @@
 class RecipesController < ApplicationController
-  def home
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @recipes = Recipe.all
   end
+
+  def show
+  end
+
+  
+  def new
+    @recipe = Recipe.new(ingredients: [Ingredient.new])
+  end
+  
+
+  def edit
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    respond_with(@recipe)
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    respond_with(@recipe)
+  end
+
+  def destroy
+    @recipe.destroy
+    respond_with(@recipe)
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).
+      permit(:name, :instructions, ingredients_attributes: 
+        [:id, :description, :quantity, :measurement, :_destroy])    
+    end
 end
