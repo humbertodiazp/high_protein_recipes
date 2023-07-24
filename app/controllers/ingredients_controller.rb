5dd9c62c3b7f58fbd27fc9e617a08fb97e8de779
Humberto <hdkaratechop@gmail.com>
2023-07-24 02:24:01 -0700

    Generate recipe and ingredients model/controllers

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,15 @@
+class IngredientsController < ApplicationController
+    before_action :setup_recipe
+
+    def new
+    end
+
+    def destroy
+    end 
+
+    private
+
+    def setup_recipe
+        @recipe = Recipe.new(ingredients: [Ingredient.new])
+    end
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,5 @@
+class Recipe < ApplicationRecord
+has_many :ingredients
+accepts_nested_attributes_for :ingredients, 
+    reject_if: :all_blank, allow_destroy: true
+end
