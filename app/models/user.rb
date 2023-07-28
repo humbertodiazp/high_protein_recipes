3b9238082b9fb370db599fb9066501c577359de3
Humberto <hdkaratechop@gmail.com>
2023-07-27 23:12:07 -0700

    Add recipe and shopping lists references. Clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,20 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable, 
           :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates_uniqueness_of :email 
+  has_many :recipes
+  has_many :shopping_lists
+
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email #assuming the user model has an email
-      user.password = Devise.friendly_token[0,20] #random password for the user
+      user.password = Devise.friendly_token[0,20] #random password the user
       user.full_name = auth.info.name #assuming the user model has a name
       user.avatar_url = auth.info.image #assuming the user model has an image
-      #if you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-    end
+    end 
   end
 
 end
