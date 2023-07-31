99665171000b8347b14d29cd2ebdb7ad22f07fc2
Humberto <hdkaratechop@gmail.com>
2023-07-30 22:40:43 -0700

    Add has_many/through shopping_list_items asstns

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,5 +1,6 @@
 class ShoppingList < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :ingredients
+  has_many :shopping_list_items
+  has_many :ingredients, through: :shopping_list_items
   validates :name, presence: true
 end
