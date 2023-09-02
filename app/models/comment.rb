c23e4d1201991fc42a9243e912ea9c4d348d1ca6
Humberto <hdkaratechop@gmail.com>
2023-09-01 23:59:32 -0700

    add comment model associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,15 @@
 class Comment < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_rich_text :body
   validates :body, presence: true
+
+  after_create_commit do
+    broadcast_append_to [commentable, :comments], target: "#{dom_id(commentable)}_comments" 
+  end 
+
+  after_destroy_commit do 
+    broadcast_remove_to [commentable, :comments], target: "#{dom_id(commentable)}_comments" 
+  end
 end
