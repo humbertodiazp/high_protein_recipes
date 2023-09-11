fee5ba8b6eea03de32689a1bcdac55aa2df5d62d
Humberto <hdkaratechop@gmail.com>
2023-09-10 22:44:13 -0700

    generate like model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :shopping_lists
   has_many :comments
+  has_many :likes, as: :record  
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
