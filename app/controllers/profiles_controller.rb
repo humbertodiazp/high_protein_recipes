a40951ed73c3e23f6d52aa4ce0aff32c72872e3b
Humberto <hdkaratechop@gmail.com>
2023-09-13 20:47:57 -0700

    add recipes to show action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,6 +13,7 @@ class ProfilesController < ApplicationController
     def show
         @user = User.find(params[:id])
         @profile = @user.profile
+        @recipes = @user.recipes
 
     end
 
