92636f33e5c7a6466e4550e0499c1e0c2e5463e2
Humberto <hdkaratechop@gmail.com>
2023-10-02 22:26:31 -0700

    Define methods for like/unlike actions in recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,5 +7,26 @@ class Recipe < ApplicationRecord
     has_one_attached :image
     has_rich_text :content 
     has_many :comments, as: :commentable
-    has_rich_text :body 
+    has_many :likes, as: :record
+
+
+    def liked_by?(user)
+        likes.where(user: user).any?
+    end
+
+    def like(user)
+        likes.where(user: user).first_or_create
+    end
+
+    def unlike(user)
+        likes.where(user: user).destroy_all
+    end
+
+    def recipe_img 
+        if self.image.attached?
+            self.image.variant(resize: '300x300!').processed
+        else
+            'recipe.jpg'
+        end
+    end
 end
