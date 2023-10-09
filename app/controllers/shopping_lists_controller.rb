eeaaab79254f084d8c44ab797e7a02288ea5194d
Humberto <hdkaratechop@gmail.com>
2023-10-08 23:54:30 -0700

    update crud actions, and params

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,13 +1,18 @@
 class ShoppingListsController < ApplicationController
     before_action :authenticate_user!
-    # before_action :set_shopping_list, only: [:show, :edit, :update, :destroy]
+    before_action :set_shopping_list, only: [:show, :edit, :update, :destroy]
+
+
     def new
-        @shopping_list = current_user.shopping_lists.build
+        @shopping_list = ShoppingList.new
         @recipe = Recipe.find(params[:recipe_id])
         
-        # Assuming your shopping_list has_many ingredients
-        @shopping_list.shopping_list_items.build
+        # Create new ShoppingListItem and Ingredients for the shopping list
+        @shopping_list.shopping_list_items.build(ingredients: @recipe.ingredients.map { |ingredient| Ingredient.new(description: ingredient.description) })
+        
+        render layout: 'no_nav'
     end
+
 
     def index
         @shopping_lists = current_user.shopping_lists
@@ -24,24 +29,73 @@ class ShoppingListsController < ApplicationController
     end
 
     def create
-        @shopping_list = current_user.shopping_lists.build(shopping_list_params)
-        if @shopping_list.save
-            respond_to do |format|
-                format.turbo_stream { render turbo_stream: turbo_stream.update('sidebar', partial: 'sidebar') }
-                format.html { redirect_to shopping_list_path(@shopping_list), notice: 'Shopping list was successfully created.' }
+        @shopping_list = current_user.shopping_lists.new(shopping_list_params)
+        respond_to do |format|
+            if @shopping_list.save 
+                format.html { redirect_to @shopping_list, notice: 'Shopping List was successfully created.' }
+                format.json { render :show, status: :created, location: @shopping_list }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @shopping_list.errors, status: :unprocessable_entity }
             end
-        else
-            render :new
         end
     end
 
+    def update
+        respond_to do |format|
+            if @shopping_list.update(shopping_list_params)
+                format.html { redirect_to @shopping_list, notice: 'Shopping List was successfully updated.' }
+                format.json { render :show, status: :ok, location: @shopping_list }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @shopping_list.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+
+    def edit
+    end 
+
+
+    # def update
+    #     respond_to do |format|
+    #         if @shopping_list.update(shopping_list_params)
+    #             format.html { redirect_to @shopping_list, notice: 'Shopping List was successfully updated.' }
+    #             format.json { render :show, status: :ok, location: @shopping_list }
+    #         else
+    #             format.html { render :edit, status: :unprocessable_entity }
+    #             format.json { render json: @shopping_list.errors, status: :unprocessable_entity }
+    #         end
+    #     end
+    # end
+
+    def destroy
+        @shopping_list.destroy
+        respond_to do |format|
+            format.html { redirect_to shopping_lists_url, notice: 'Shopping List was successfully deleted.' }
+            format.json { head :no_content }
+        end
+    end
+
+
     private
 
-    # def set_shopping_list
-    #     @shopping_list = current_user.shopping_lists.find(params[:id])
-    # end
-    
-    def shopping_list_params
-        params.require(:shopping_list).permit(:name, shopping_list_item_ids: [])
+
+    def set_shopping_list
+        @shopping_list = current_user.shopping_lists.find(params[:id])
     end
-end
+    
+
+
+    def shopping_list_params
+        params.require(:shopping_list).permit(
+            :name,
+            shopping_list_items_attributes: [
+            :id,
+            ingredients_attributes: [:id, :description, :quantity, :measurement, :_destroy]
+            ]
+        )
+    end
+end 
+
