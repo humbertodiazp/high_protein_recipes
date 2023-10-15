ed660f2c47f1f7c0b099868f154f4cdc1a9cd86e
Humberto <hdkaratechop@gmail.com>
2023-10-14 23:53:27 -0700

    Update actions as through table no longer needed

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,101 +1,56 @@
 class ShoppingListsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_shopping_list, only: [:show, :edit, :update, :destroy]
-
-
-    def new
-        @shopping_list = ShoppingList.new
-        @recipe = Recipe.find(params[:recipe_id])
-        
-        # Create new ShoppingListItem and Ingredients for the shopping list
-        @shopping_list.shopping_list_items.build(ingredients: @recipe.ingredients.map { |ingredient| Ingredient.new(description: ingredient.description) })
-        
-        render layout: 'no_nav'
-    end
-
-
+  
     def index
-        @shopping_lists = current_user.shopping_lists
+      @shopping_lists = current_user.shopping_lists
     end
-
+  
     def show
-        @shopping_list = current_user.shopping_lists.find_by(id: params[:id])
-
-        if @shopping_list.nil?
-            redirect_to shopping_lists_path, alert: 'Shopping list not found.'
-        else
-            @other_shopping_lists = current_user.shopping_lists.where.not(id: @shopping_list.id)
-        end
+      # You can display the shopping list and its items here.
+      render layout: 'no_nav'
     end
-
+  
+    def new
+      @shopping_list = ShoppingList.new
+    end
+  
     def create
-        @shopping_list = current_user.shopping_lists.new(shopping_list_params)
-        respond_to do |format|
-            if @shopping_list.save 
-                format.html { redirect_to @shopping_list, notice: 'Shopping List was successfully created.' }
-                format.json { render :show, status: :created, location: @shopping_list }
-            else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @shopping_list.errors, status: :unprocessable_entity }
-            end
-        end
+      @shopping_list = current_user.shopping_lists.new(shopping_list_params)
+      if @shopping_list.save
+        redirect_to shopping_list_path(@shopping_list), notice: 'Shopping list created successfully.'
+      else
+        render :new
+      end
     end
-
-    def update
-        respond_to do |format|
-            if @shopping_list.update(shopping_list_params)
-                format.html { redirect_to @shopping_list, notice: 'Shopping List was successfully updated.' }
-                format.json { render :show, status: :ok, location: @shopping_list }
-            else
-                format.html { render :edit, status: :unprocessable_entity }
-                format.json { render json: @shopping_list.errors, status: :unprocessable_entity }
-            end
-        end
-    end
-
-
+  
     def edit
-    end 
+      render layout: 'no_nav'
+    end
+  
+    def update
 
+      if @shopping_list.update(shopping_list_params)
+        redirect_to shopping_list_path(@shopping_list), notice: 'Shopping list updated successfully.'
+      else
+        render :edit
+      end
 
-    # def update
-    #     respond_to do |format|
-    #         if @shopping_list.update(shopping_list_params)
-    #             format.html { redirect_to @shopping_list, notice: 'Shopping List was successfully updated.' }
-    #             format.json { render :show, status: :ok, location: @shopping_list }
-    #         else
-    #             format.html { render :edit, status: :unprocessable_entity }
-    #             format.json { render json: @shopping_list.errors, status: :unprocessable_entity }
-    #         end
-    #     end
-    # end
-
+    end
+  
     def destroy
-        @shopping_list.destroy
-        respond_to do |format|
-            format.html { redirect_to shopping_lists_url, notice: 'Shopping List was successfully deleted.' }
-            format.json { head :no_content }
-        end
+      @shopping_list.destroy
+      redirect_to shopping_lists_path, notice: 'Shopping list deleted successfully.'
     end
-
-
+  
     private
-
-
+  
     def set_shopping_list
-        @shopping_list = current_user.shopping_lists.find(params[:id])
+      @shopping_list = current_user.shopping_lists.find(params[:id])
     end
-    
-
-
+  
     def shopping_list_params
-        params.require(:shopping_list).permit(
-            :name,
-            shopping_list_items_attributes: [
-            :id,
-            ingredients_attributes: [:id, :description, :quantity, :measurement, :_destroy]
-            ]
-        )
+      params.require(:shopping_list).permit(:name)
     end
-end 
-
+end
+  
