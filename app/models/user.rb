0f8470e98e02fa4747326cea0a54a213c0c1eccf
Humberto <hdkaratechop@gmail.com>
2023-11-05 17:18:45 -0800

    Define follow/unfollow via relationship status

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,19 @@ class User < ApplicationRecord
   has_many :shopping_lists
   has_many :comments
   has_many :likes, as: :record  
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  has_one :profile, dependent: :destroy 
+
+
+  
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -16,6 +29,21 @@ class User < ApplicationRecord
       user.full_name = auth.info.name #assuming the user model has a name
       user.avatar_url = auth.info.image #assuming the user model has an image
     end 
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 end
