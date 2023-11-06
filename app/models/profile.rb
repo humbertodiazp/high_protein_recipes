c5d2c09698f2baba89383ffbfb06fa7939682989
Humberto <hdkaratechop@gmail.com>
2023-11-05 17:19:22 -0800

    Add color setting params for user cover img

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,17 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  validates :name , presence: true
+  has_one_attached :image 
 
+  after_create :set_color
 
+  def set_color
+    self.color = color_list.sample
+    save
+  end
+
+  def color_list
+    %w[red orange yellow green cyan lightblue darkblue purple pink]
+  end
 
 end
