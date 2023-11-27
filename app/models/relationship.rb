4c5a8645a0555cea3bc5c273048b637f8c949ce0
Humberto <hdkaratechop@gmail.com>
2023-11-26 18:13:26 -0800

    add notifications to relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,12 @@ class Relationship < ApplicationRecord
     belongs_to :followed, class_name: "User"
     validates :follower_id, presence: true
     validates :followed_id, presence: true
+
+    has_noticed_notifications
+
+    after_create_commit :notify_user
+
+    def notify_user
+        RelationshipNotification.with(relationship: self).deliver_later(followed)
+    end 
 end
