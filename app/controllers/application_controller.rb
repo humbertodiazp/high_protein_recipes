8bec6ebdb57269f659fad1c295dbf1e87c76a747
Humberto <hdkaratechop@gmail.com>
2023-11-26 18:07:11 -0800

    configure devise settings/ def set_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,29 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  # before_action :set_current_user
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   self.responder = ApplicationResponder
   respond_to :html
 
-end
+
+  def set_current_user
+    if session[:user_id]
+        Current.user = User.find_by(id: session[:user_id])
+    end
+  end
+
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name terms_and_conditions])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name ])
+  end 
+end 
+
+
