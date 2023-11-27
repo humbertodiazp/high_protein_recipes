cbb044b6cb86c4ad48287bad122ac2283257db37
Humberto <hdkaratechop@gmail.com>
2023-11-26 18:14:01 -0800

    remove non-applicable associations

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,6 +1,5 @@
 class ShoppingList < ApplicationRecord
   belongs_to :user
   has_many :shopping_list_items
-  has_many :ingredients, through: :shopping_list_items
-  validates :name, presence: true
 end
+
