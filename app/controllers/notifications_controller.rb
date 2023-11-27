e865d1a13bafa23f5704a015b9fc37a30ec8cbf6
Humberto <hdkaratechop@gmail.com>
2023-11-26 18:08:23 -0800

    generate notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = Notification.where(recipient: current_user)
+    render layout: 'no_nav'
+    # en.notifications.relationship.message
   end
 end
