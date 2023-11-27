9884e454c7340f891fe72604e987f7866ca36e35
Humberto <hdkaratechop@gmail.com>
2023-11-26 18:13:07 -0800

    remove recipe_image method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,11 @@
 class Recipe < ApplicationRecord
     belongs_to :user
-    has_many :ingredients, dependent: :destroy
+    has_many :ingredients
     accepts_nested_attributes_for :ingredients, 
         reject_if: :all_blank, allow_destroy: true
-    validates :content, presence: true
     has_one_attached :image
     has_rich_text :content 
+    validates :content, presence: true
     has_many :comments, as: :commentable
     has_many :likes, as: :record
 
@@ -20,13 +20,5 @@ class Recipe < ApplicationRecord
 
     def unlike(user)
         likes.where(user: user).destroy_all
-    end
-
-    def recipe_img 
-        if self.image.attached?
-            self.image.variant(resize: '300x300!').processed
-        else
-            'recipe.jpg'
-        end
     end
 end
