135496aeec207573cc251dc09f0637093eaee225
Humberto <hdkaratechop@gmail.com>
2023-12-03 22:59:20 -0800

    add notifications to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,11 +6,20 @@ class Comment < ApplicationRecord
   belongs_to :parent, optional: true, class_name: "Comment"
   has_many :comments, foreign_key: :parent_id, dependent: :destroy
 
+  has_noticed_notifications 
+
   has_rich_text :body
 
   validates :body, presence: true
 
+  # after_create_commit :notify_user
+
+  def notify_user
+    CommentNotification.with(comment: self).deliver_later(user)
+  end
+
   after_create_commit do
+    notify_user 
     broadcast_append_to [commentable, :comments], target: "#{dom_id(parent || commentable)}_comments", partial: "comments/comment_with_replies"
   end
 
