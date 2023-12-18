71bc491d1d26acd06f138b97559fbb4fa1037528
Humberto <hdkaratechop@gmail.com>
2023-12-17 17:41:34 -0800

    Enable notifications for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,21 +6,25 @@ class Comment < ApplicationRecord
   belongs_to :parent, optional: true, class_name: "Comment"
   has_many :comments, foreign_key: :parent_id, dependent: :destroy
 
-  has_noticed_notifications 
+  has_noticed_notifications
 
   has_rich_text :body
 
   validates :body, presence: true
 
-  # after_create_commit :notify_user
+  after_create_commit :notify_user
 
   def notify_user
-    CommentNotification.with(comment: self).deliver_later(user)
+    recipe_owner = commentable.user
+
+    # Check if the comment's user is different from the recipe's owner
+    if user != recipe_owner
+      CommentNotification.with(comment: self).deliver_later(recipe_owner)
+    end
   end
 
   after_create_commit do
     broadcast_append_to [commentable, :comments], target: "#{dom_id(parent || commentable)}_comments", partial: "comments/comment_with_replies"
-    notify_user
   end
 
   after_update_commit do
