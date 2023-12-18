bb33fd958428ca928b27e51de6d57cd31e96a9c4
Humberto <hdkaratechop@gmail.com>
2023-12-17 16:31:01 -0800

    Set edit to act as new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,26 +3,29 @@ class ProfilesController < ApplicationController
     before_action :set_profile
     before_action :authenticate_user!
 
+
+
     def edit
         @profile = current_user.profile || current_user.build_profile
         render layout: 'no_nav'
     end
 
     def create
+
         @profile = current_user.build_profile(profile_params)
         if @profile.save
         redirect_to @profile
         else
-        render :new, layout: 'no_nav'
+            render :edit, layout: "no_nav", status: :unprocessable_entity
         end
-        render layout: 'no_nav'
+
     end
     
     def update
         if @profile.update(profile_params)
         redirect_to @profile, notice: 'Profile was successfully updated.'
         else
-        render :edit, layout: 'no_nav'
+        redirect_to @profile, notice: 'Profile was not updated.'
         end
     end
     
