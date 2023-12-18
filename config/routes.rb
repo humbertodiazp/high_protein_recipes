12211c6ccbfe89ec5b49db5f7321a92b9f5f8743
Humberto <hdkaratechop@gmail.com>
2023-12-17 18:46:49 -0800

    Def ingredients/shop list route. organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,59 @@ Rails.application.routes.draw do
               controllers: {
                 registrations: 'users/registrations',
                 sessions: 'users/sessions',
-                omniauth_callbacks: 'users/omniauth_callbacks'
+                omniauth_callbacks: 'users/omniauth_callbacks',
+                # user: 'users'
                 }
   
-  
-
-  
+#profile routes
+  resources :users, only: [] do
+    resources :profiles
+  end
+#follow routes
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+#recipe routes for comments and likes
   resources :recipes do 
     resources :comments, module: :recipes
     resource :like, module: :recipes
   end
-
+#recipe route to add ingredients to shopping list
+  resources :recipes do
+    member do
+      post 'add_to_shopping_list'
+    end
+  end
+#comment routes for comments on comments
   resources :comments do
     resources :comments, module: :comments
   end
-  
-  resources :shopping_lists
-  resources :shopping_list_items
-  resources :users, only: [:show]
-
+  #shopping list routes for shopping list items and ingredients
+  resources :shopping_lists do
+    resources :shopping_list_item
+    # resources :ingredients, only: [:new, :create, :destroy]
+  end
+#ingredient routes to add/remove ingredients from recipe
   resources :ingredients, only: [], param: :index do
     member do
       delete '(:id)' => "ingredients#destroy", as: ""
       post '/' => "ingredients#create"
     end
   end
+
+  #general routes
+  resources :shopping_list_items
+  resources :users
+  resources :profiles 
+  resources :recipes 
+
+  # relationship and follow routes
+  resources :relationships, only: [:create, :destroy]
+  resources :follows, only: [:create, :destroy]
+
+  # notification routes
+  resources :notifications, only: [:index, :update, :show]
+
 end 
