d7b53644e3209dca478bae6ad042f1aedfe11bbe
Humberto <hdkaratechop@gmail.com>
2023-12-17 16:24:23 -0800

    Render no_nav for show action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,38 @@
-class CommentsController < ApplicationController 
+class CommentsController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_comment 
-
+    before_action :set_comment
+  
     def show
+      render layout: 'no_nav'
     end
-
+  
     def edit
     end
-
+  
     def update
-        if @comment.update(comment_params)
-            redirect_to @comment
-        else 
-            render :edit, status: :unprocessable_entity
-        end
+      if @comment.update(comment_params)
+        redirect_to @comment
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
-
+  
     def destroy
-        @comment.destroy
-        respond_to do |format|
-            format.turbo_stream {}
-            format.html { redirect_to @comment.commentable }
-        end
+      @comment.destroy
+      respond_to do |format|
+        format.turbo_stream {}
+        format.html { redirect_to @comment.commentable }
+      end
     end
-
+  
     private
+  
     def set_comment
-        @comment = current_user.comments.find(params[:id])
+      @comment = current_user.comments.find(params[:id])
     end
-    
+  
     def comment_params
-        params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body)
     end
-end 
+  end
+  
