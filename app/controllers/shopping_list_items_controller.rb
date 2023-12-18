f116b61b172a61a330dd12eb22f895c96b2e26af
Humberto <hdkaratechop@gmail.com>
2023-12-17 18:50:49 -0800

    Clean up controller

--- app/controllers/shopping_list_items_controller.rb
+++ app/controllers/shopping_list_items_controller.rb
@@ -1,50 +1,23 @@
-class ShoppingListItemsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_shopping_list
-    before_action :set_shopping_list_item, only: [:destroy]
-  
-    def new_ingredient
-      @shopping_list_item = @shopping_list.shopping_list_items.build
-    end
-    
+class ShoppingListItemsController< ApplicationController
+  before_action :setup_shopping_list 
 
-    def create
-      @shopping_list_item = @shopping_list.shopping_list_items.build(ingredient_params)
-      if @shopping_list_item.save
-        redirect_to edit_shopping_list_path(@shopping_list), notice: 'Ingredient added to the shopping list.'
-      else
-        render :new_ingredient
-      end
-    end
-  
-    def destroy
-      @ingredient = ShoppingListItem.find(params[:id])
-      @shopping_list = @ingredient.shopping_list
-    
-      if @ingredient.destroy
-        render turbo_stream: [
-          turbo_stream.remove("ingredient_#{@ingredient.id}")
-        ]
-      else
-        redirect_to edit_shopping_list_path(@shopping_list), alert: 'Ingredient could not be removed from the shopping list.'
-      end
-    end
-    
-    
-    
-  
-    private
-  
-    def set_shopping_list
-      @shopping_list = current_user.shopping_lists.find(params[:shopping_list_id])
-    end
-  
-    def set_shopping_list_item
-      @shopping_list_item = @shopping_list.shopping_list_items.find(params[:id])
-    end
-  
-    def ingredient_params
-      params.require(:ingredient).permit(:description)
-    end
+  def new
   end
+
+  def destroy
+      @shopping_list.shopping_list_items.first.destroy
+      redirect_to edit_shopping_list(@shopping_list) 
+  end 
   
+  def edit
+  end 
+
+  def update
+  end 
+
+  private
+
+  def set_up_shopping_list
+      @shopping_list = Shopping_list.new(shopping_list_items: [Shopping_list_item.new])
+  end
+end
