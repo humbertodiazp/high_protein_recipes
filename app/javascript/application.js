b3d35877f642ecc012b624bf96a3272187546292
Humberto <hdkaratechop@gmail.com>
2023-12-17 18:33:53 -0800

    rearrange import map

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,35 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "trix"
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-
-
-
-import "trix"
 import "@rails/actiontext"
+// import "./custom/menu"
+// import "./custom/nav_links"
+
+
+//  import { Application } from "stimulus";
+// import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+// const application = Application.start();
+// const context = require.context("controllers", true, /_controller\.js$/);
+// application.load(definitionsFromContext(context));
+
+
+//  import { Turbo } from "@hotwired/turbo-rails";
+
+// Turbo.StreamActions.redirect = function () {
+//     Turbo.visit(this.target);
+// };
+
+// // or using event listener
+
+// document.addEventListener("turbo:before-stream-render", (event) => {
+//     const fallbackToDefaultActions = event.detail.render;
+//     event.detail.render = function (streamElement) {
+//         if (streamElement.action == "redirect") {
+//             Turbo.visit(streamElement.target);
+//         } else {
+//             fallbackToDefaultActions(streamElement);
+//         }
+//     };
+// });
