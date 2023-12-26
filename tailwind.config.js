515f709bfbec1b587f3fed44f1e0de48b8f4bd33
Humberto <hdkaratechop@gmail.com>
2023-12-25 22:37:02 -0800

    add trix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    'trix/**/*.js'
   ],
   plugins: [
     require('@tailwindcss/forms'),
