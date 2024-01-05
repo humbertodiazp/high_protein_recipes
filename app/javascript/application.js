bc2867794b7086fa1271eab5607a4d0908444082
Humberto <hdkaratechop@gmail.com>
2024-01-05 00:43:26 -0800

    try to fix trix bug attempt # 2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
-require ("trix")
-require ("@rails/actiontext")
+import "trix"
+import "@rails/actiontext"
 
 
 // import "@hotwired/turbo-rails";
