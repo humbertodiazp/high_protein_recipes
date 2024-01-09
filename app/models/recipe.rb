3e282e989dea0c1cdd1bc748ba4343ecb7a5235f
Humberto <hdkaratechop@gmail.com>
2024-01-08 19:33:33 -0800

    Fix rich text area in comment and recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,8 +4,8 @@ class Recipe < ApplicationRecord
     accepts_nested_attributes_for :ingredients, 
         reject_if: :all_blank, allow_destroy: true
     has_one_attached :image
-    # has_rich_text :content 
-    validates :content, presence: true
+    has_rich_text :content 
+    validates :body, presence: true
     has_many :comments, as: :commentable
     has_many :likes, as: :record
 
